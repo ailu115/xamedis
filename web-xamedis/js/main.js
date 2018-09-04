411cb14022294db51c2678d3d670332c863379b6
Ailen Colavecchia <ailencolavecchia@gmail.com>
2018-09-03 23:51:52 -0300

    arme una carpeta nueva que se llama web-xamedis donde estoy armando toda la pagina sin el responsive el resto quedo afuera es un backup

---
+++ web-xamedis/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    
+    
+/*
+    
+    $(".btn-menu").click(function(){
+  $( "nav" ).slideToggle("slow");
+});
+    
+*/ 
+
+    
+    
+  
+ $( ".btn-menu" ).click(function() {
+
+     
+     if($('.menu').hasClass('open')) { 
+         $('.menu').removeClass('open');
+     } else { 
+        $('.menu').addClass('open');
+     }
+     
+     
+});
+
+   
+  
+    
+    
+
+    
+    
+})
+
+
+
